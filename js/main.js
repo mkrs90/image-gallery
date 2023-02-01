6976ee42d9a340bb5058aee182811c3ddc032f00
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-02-01 16:20:22 +0000

    added foundation for the for loop

--- js/main.js
+++ js/main.js
@@ -5,10 +5,25 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
+const altTestArray = {
+    'pic1.jpg' : 'closeup of a human eye',
+    'pic2.jpg' : 'arial view of the dessert', 
+    'pic3.jpg' : 'closeup of purple and white flowers', 
+    'pic4.jpg' : 'ancient Egyptian hieroglyphics', 
+    'pic5.jpg' : 'closeup of moth on a leaf'
+};
+/* Looping through images Loop through the array of filenames, 
+    and for each one, insert an <img> element inside the thumb-bar <div>
+    that embeds that image in the page along with its alternative text. */
+for (const image of imageArray) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `/css/img/${image}`);
+    newImage.setAttribute('alt', altTestArray[image]);
+    thumbBar.appendChild(newImage);
+}
 
-/* Looping through images */
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
---
+++ pseudocode.md
@@ -0,0 +1,16 @@
+# Pseudocode for Image Gallery
+
+Main features 
+- One main image
+- Remaining images reside at bottom
+- you can click on the images to make it the main image
+- you can lighten or darken the main image
+
+
+
+- Display the image
+- thumbnail additional images
+    - additional images put in an array
+    - additional alt text put into an array
+- lighten or darken image with button
+- 
