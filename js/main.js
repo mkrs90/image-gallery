665537f360d6a0ca7f57d12ac1b080a108cfa9d9
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-02-02 21:30:12 +0000

    changed photos

--- js/main.js
+++ js/main.js
@@ -5,14 +5,20 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imageArray = [
+    'pic1.png', 
+    'pic2.png', 
+    'pic3.png', 
+    'pic4.png', 
+    'pic5.png'
+];
 /* Declaring the alternative text for each image file */
 const altTestArray = {
-    'pic1.jpg' : 'closeup of a human eye',
-    'pic2.jpg' : 'arial view of the dessert', 
-    'pic3.jpg' : 'closeup of purple and white flowers', 
-    'pic4.jpg' : 'ancient Egyptian hieroglyphics', 
-    'pic5.jpg' : 'closeup of moth on a leaf'
+    'pic1.png' : 'closeup of a human eye',
+    'pic2.png' : 'arial view of the dessert', 
+    'pic3.png' : 'closeup of purple and white flowers', 
+    'pic4.png' : 'ancient Egyptian hieroglyphics', 
+    'pic5.png' : 'closeup of moth on a leaf'
 }
 /* Looping through images Loop through the array of filenames, 
     and for each one, insert an <img> element inside the thumb-bar <div>
